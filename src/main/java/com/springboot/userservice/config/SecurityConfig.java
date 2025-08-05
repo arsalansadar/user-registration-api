de6eb05f660b3d81d1b0d6b1fec42a8091b50f1b
Arsalan <asadar1628@gmail.com>
2025-08-05 13:08:04 +0530

    fix: add @Configuration annotation to SecurityConfig to register BCryptPasswordEncoder bean

--- src/main/java/com/springboot/userservice/config/SecurityConfig.java
+++ src/main/java/com/springboot/userservice/config/SecurityConfig.java
@@ -1,8 +1,10 @@
 package com.springboot.userservice.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+@Configuration
 public class SecurityConfig {
 
     @Bean
